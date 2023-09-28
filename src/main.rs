b06468c0990dbe32e5eddab5c4a1c007a9032f4c
Daniel Temiagin <talk2drys@gmail.com>
2023-09-27 21:53:24 -0600

    add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+mod configuration;
+use tracing::{debug, info};
+
+#[actix::main]
+async fn main() {
+    let subscriber = tracing_subscriber::FmtSubscriber::default();
+    tracing::subscriber::set_global_default(subscriber).expect("error setting global subscriber");
+
+    info!("reading configuraion file");
+
     println!("Hello, world!");
 }
